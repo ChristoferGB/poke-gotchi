964d1c2e8063f9db32393a932b2fd4bb6e57663a
christofer-bernardo <christofergaldino2@hotmail.com>
2024-03-19 15:38:21 -0300

    Adjusts view to improve layout

--- View/PokegotchiView.cs
+++ View/PokegotchiView.cs
@@ -7,14 +7,27 @@ namespace poke_gotchi.View
     {
         public void Welcome()
         {
+            Console.WriteLine("=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=");
+            Console.WriteLine(@"
+ _____        _                        _         _      _ 
+|  __ \      | |                      | |       | |    (_)
+| |__) |___  | | __ ___   __ _   ___  | |_  ___ | |__   _ 
+|  ___// _ \ | |/ // _ \ / _` | / _ \ | __|/ __|| '_ \ | |
+| |   | (_) ||   <|  __/| (_| || (_) || |_| (__ | | | || |
+|_|    \___/ |_|\_\\___| \__, | \___/  \__|\___||_| |_||_|
+                          __/ |                           
+                         |___/                            
+");
+            Console.WriteLine("=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=\n");
+
             Console.WriteLine("Welcome! Here you'll be able to choose your lovely friend, your Pokemon!");
             Console.Write("To begin, please tell us your name: ");
         }
 
         public void MainMenu(string username)
         {
-            Console.WriteLine("\n");
-            Console.WriteLine("Main Menu");
+            Console.WriteLine("------------------------------------------------------------------------\n");
+            Console.WriteLine("Main Menu\n");
             Console.WriteLine($"{username}, what do you wish?");
             Console.WriteLine("1 - Adopt a new pokemon");
             Console.WriteLine("2 - View adopted pokemon");
@@ -23,7 +36,7 @@ namespace poke_gotchi.View
 
         public void ChoosePokemonMenu(PokemonListResponse pokemonList)
         {
-            Console.WriteLine("\n");
+            Console.WriteLine("------------------------------------------------------------------------\n");
             Console.WriteLine("Choose a Pokemon from the list below:");
 
             for (int i = 0; i < pokemonList?.Results.Count; i++)
@@ -34,7 +47,7 @@ namespace poke_gotchi.View
 
         public void AdoptPokemonMenu(string username, string pokemonName)
         {
-            Console.WriteLine("\n");
+            Console.WriteLine("------------------------------------------------------------------------\n");
             Console.WriteLine($"{username}, would you like to: ");
             Console.WriteLine($"1 - View information about {pokemonName}");
             Console.WriteLine($"2 - Adopt {pokemonName}");
@@ -43,7 +56,7 @@ namespace poke_gotchi.View
 
         public void PokemonInformation(Pokemon pokemon, List<string> abilitiesNames)
         {
-            Console.Write("\n");
+            Console.WriteLine("------------------------------------------------------------------------\n");
             Console.WriteLine("Chosen Pokemon \n");
 
             Console.WriteLine($"Name: {pokemon?.Name}");
@@ -55,27 +68,33 @@ namespace poke_gotchi.View
             {
                 Console.WriteLine(abilityName);
             }
+
+            Console.WriteLine("\n");
         }
 
         public void AdoptedPokemonMessage(string username, string pokemonName)
         {
-            Console.WriteLine($"{username}, you have adopted {pokemonName} successfully!");
+            Console.WriteLine($"{username}, you have adopted {pokemonName} successfully!\n");
         }
 
         public void AdoptedPokemonList(List<AdoptedPokemon> pokemonList, string username)
         {
+            Console.WriteLine("------------------------------------------------------------------------\n");
+            Console.WriteLine("Adopted Pokemon\n");
+
             for (int i = 0; i < pokemonList.Count; i++)
             {
                 Console.WriteLine($"{i + 1} - {pokemonList[i].Name}");
             }
 
+            Console.WriteLine("\n");
             Console.WriteLine($"{username}, if you wish to interact with one of your pokemon, type its number.");
             Console.WriteLine("If you want to return, just type anything else.");
         }
 
         public void InteractPokemonMenu(string username, string pokemonName)
         {
-            Console.WriteLine("\n");
+            Console.WriteLine("------------------------------------------------------------------------\n");
             Console.WriteLine($"{username}, would you like to: ");
             Console.WriteLine($"1 - Know how {pokemonName} is doing");
             Console.WriteLine($"2 - Feed {pokemonName}");
